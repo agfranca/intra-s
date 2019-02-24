01dd76cf721294c0ab6ef0f0671f3ce3cd94451f
agfranca <alexandre_franca@hotmail.com>
2019-02-24 00:53:14 -0300

    Create 2019_02_24_004513_add_image_table_users.php

---
+++ database/migrations/2019_02_24_004513_add_image_table_users.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddImageTableUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+        $table->dropColumn('arquivo_id');
+		});
+		
+		Schema::table('users', function (Blueprint $table) {
+        $table->integer('arquivo_id') // Nome da coluna
+                    ->nullable() // Preenchimento não obrigatório
+                    ->after('password'); // Ordenado após a coluna "password"
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+        $table->dropColumn('arquivo_id');
+		});
+    }
+}
