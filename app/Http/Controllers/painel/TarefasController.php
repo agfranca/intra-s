a7d70f3f6ccab77b363e009cac16d70d151aedd3
agfranca <alexandre_franca@hotmail.com>
2019-06-06 08:42:15 -0300

    Update TarefasController.php

--- app/Http/Controllers/painel/TarefasController.php
+++ app/Http/Controllers/painel/TarefasController.php
@@ -602,8 +602,8 @@ class TarefasController extends Controller
             $iPreviewConfig .= "{type:'$tipo', url:'$urlDelete', downloadUrl:'$url', width: \"120px\", key:$key,},"; } 
 
         //dd($idTarefa);
-        dd($urls);
-        //dd($iPreview);    
+        //dd($urls);
+        dd($iPreview);    
         //dd($iPreviewConfig);    
         return view('painel.tarefas.anexos',compact('idTarefa','urls','iPreview','iPreviewConfig'));
 
